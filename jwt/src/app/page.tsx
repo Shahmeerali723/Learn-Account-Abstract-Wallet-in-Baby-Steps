00e93673c2b6bdbe50fc32f84604c2560f122f9a
Shahmeerali723 <shahmeer.cyber@gmail.com>
2023-08-12 15:16:30 -0700

    Update

--- jwt/src/app/page.tsx
+++ jwt/src/app/page.tsx
@@ -1,67 +1,14 @@
-function RpcProviderExample() {
-    const [jwt, setJWT] = useState('')
-    const [address, setAddress] = useState('')
-    const [loading, setLoading] = useState(false)
-    const userId = window.crypto.getRandomValues(new Uint32Array(4)).join('-')
+import { getZeroDevSigner, getRPCProviderOwner } from '@zerodevapp/sdk'
+import { ZeroDevWeb3Auth, ZeroDevWeb3AuthWithModal } from '@zerodevapp/web3auth';
 
-    resetJWT = () => {
-        fetch(`https://jwt-issuer.onrender.com/create-jwt/${userId}`).then(response => {
-            response.text().then(setJWT)
-        })
-    }
+let signer: ZeroDevSigner
 
-    useEffect(() => {
-        // THIS IS DEMO CODE TO CREATE A JWT, YOU WOULD HAVE YOUR OWN WAY TO GET YOUR JWT
-        resetJWT()
-    }, [])
-
-    const setWallet = async (provider) => {
-        const signer = await getZeroDevSigner({
-            projectId: defaultProjectId,
-            owner: await getRPCProviderOwner(provider)
-        })
-        setAddress(await signer.getAddress())
-    }
-
-    const zeroDevWeb3Auth = useMemo(() => {
-        const instance = new ZeroDevWeb3Auth([defaultProjectId])
-        instance.init({onConnect: async () => {
-            setLoading(true)
-            setWallet(zeroDevWeb3Auth.provider)
-            setLoading(false)
-        }})
-        return instance
-    }, [])
-
-  const disconnect = async () => {
-    await zeroDevWeb3Auth.logout()
-    setAddress('')
-    resetJWT()
-  }
-
-  const handleClick = async () => {
-    setLoading(true)
-    zeroDevWeb3Auth.connect('jwt', {jwt}).then(provider => {
-      setWallet(provider)
-    }).finally(() => {
-      setLoading(false)
-    })
-  }
-
-  const connected = !!address
-  return (
-    <div>
-      {connected && 
-        <div>
-          <label>Wallet: {address}</label>
-        </div>
-      }
-      <div>
-        {!connected && <button onClick={handleClick} disabled={loading || !jwt}>{ loading ? 'loading...' : 'Create Wallet with JWT'}</button>}
-        {connected && 
-          <button onClick={disconnect} disabled={loading}>Disconnect</button>
-        }
-      </div>
-    </div>
-  )
-}
+const zeroDevWeb3AuthNoModal = new ZeroDevWeb3Auth(['<project-id>'])
+zeroDevWeb3AuthNoModal.init({onConnect: async () => {
+  signer = await getZeroDevSigner({
+    projectId: "<project id>",
+    owner: await getRPCProviderOwner(ZeroDevWeb3Auth.provider),
+  })
+}})
+// 'google' | 'facebook' | 'twitter' | 'discord' | 'github' | 'twitch'
+ZeroDevWeb3Auth.connect('google')
